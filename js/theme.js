079add92bfae29bda440a098c576e36ac5dae2a1
TrevorStubblefield <Trevor.stubblefield1995@gmail.com>
2017-04-09 22:01:54 -0600

    JTC372

--- js/theme.js
+++ js/theme.js
@@ -1,7 +1,4 @@
 (function($) {
-    "use strict"; // Start of use strict
-
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
@@ -9,26 +6,18 @@
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
-
-    // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
         target: '.navbar-fixed-top',
         offset: 51
     });
-
-    // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
         $('.navbar-toggle:visible').click();
     });
-
-    // Offset for Main Navigation
     $('#mainNav').affix({
         offset: {
             top: 100
         }
     })
-
-    // Initialize and Configure Scroll Reveal Animation
     window.sr = ScrollReveal();
     sr.reveal('.sr-icons', {
         duration: 600,
@@ -44,8 +33,6 @@
         scale: 0.3,
         distance: '0px'
     }, 300);
-
-    // Initialize and Configure Magnific Popup Lightbox Plugin
     $('.popup-gallery').magnificPopup({
         delegate: 'a',
         type: 'image',
@@ -54,11 +41,11 @@
         gallery: {
             enabled: true,
             navigateByImgClick: true,
-            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+            preload: [0, 1]
         },
         image: {
             tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
         }
     });
 
-})(jQuery); // End of use strict
+})(jQuery);
